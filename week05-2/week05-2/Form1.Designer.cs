a0d320a2f8c02ed57c5d3d19080a2296cca241df
Tuğra GÜNDÜZ <trgndz1999@gmail.com>
2022-04-12 19:08:18 +0300

    Roll Dice

---
+++ week05-2/week05-2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace week05_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRollDice = new System.Windows.Forms.Button();
+            this.labelFirstNum = new System.Windows.Forms.Label();
+            this.labelSecNum = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRollDice
+            // 
+            this.btnRollDice.Location = new System.Drawing.Point(17, 13);
+            this.btnRollDice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRollDice.Name = "btnRollDice";
+            this.btnRollDice.Size = new System.Drawing.Size(133, 35);
+            this.btnRollDice.TabIndex = 0;
+            this.btnRollDice.Text = "Roll Dice";
+            this.btnRollDice.UseVisualStyleBackColor = true;
+            this.btnRollDice.Click += new System.EventHandler(this.btnRollDice_Click);
+            // 
+            // labelFirstNum
+            // 
+            this.labelFirstNum.BackColor = System.Drawing.Color.Maroon;
+            this.labelFirstNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelFirstNum.ForeColor = System.Drawing.Color.White;
+            this.labelFirstNum.Location = new System.Drawing.Point(13, 53);
+            this.labelFirstNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelFirstNum.Name = "labelFirstNum";
+            this.labelFirstNum.Size = new System.Drawing.Size(51, 52);
+            this.labelFirstNum.TabIndex = 1;
+            this.labelFirstNum.Text = "1";
+            this.labelFirstNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelSecNum
+            // 
+            this.labelSecNum.BackColor = System.Drawing.Color.Maroon;
+            this.labelSecNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSecNum.ForeColor = System.Drawing.Color.White;
+            this.labelSecNum.Location = new System.Drawing.Point(99, 53);
+            this.labelSecNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSecNum.Name = "labelSecNum";
+            this.labelSecNum.Size = new System.Drawing.Size(51, 52);
+            this.labelSecNum.TabIndex = 2;
+            this.labelSecNum.Text = "1";
+            this.labelSecNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelResult
+            // 
+            this.labelResult.Location = new System.Drawing.Point(168, 9);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(261, 467);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.UseMnemonic = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 485);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelSecNum);
+            this.Controls.Add(this.labelFirstNum);
+            this.Controls.Add(this.btnRollDice);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRollDice;
+        private System.Windows.Forms.Label labelFirstNum;
+        private System.Windows.Forms.Label labelSecNum;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ week05-2/week05-2/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week05_2
+{
+    public partial class Form1 : Form
+    {
+        Status gameStatus = Status.Won;
+        int myPoint = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRollDice_Click(object sender, EventArgs e)
+        {
+            /* My Codes
+            Random random = new Random((int)DateTime.Now.Ticks);
+            int firstNum = random.Next(1,6+1);
+            int secondNum = random.Next(1,6+1);
+            labelFirstNum.Text = String.Format("{0}", firstNum);
+            labelSecNum.Text = String.Format("{0}", secondNum);
+            int sum = 0;
+            sum = firstNum + secondNum;
+            if (sum == 7 || sum == 11)
+            {
+                labelResult.Text = String.Format("Sum of Dices is {0} \nYou won", sum);
+            }
+            else if (sum == 2 || sum == 3 || sum == 12)
+            {
+                labelResult.Text = String.Format("Sum of Dices is {0} \nYou Lose", sum);
+            }
+            else
+            {
+                labelResult.Text = String.Format("Sum of Dices is {0} \nYour point is {1} Roll Again", sum, sum);
+            }
+            */
+
+            /* Lecturer Codes */
+            int sumOfDice = RollDice();
+            if (gameStatus == Status.Continue)
+            {
+                if (sumOfDice == myPoint)
+                {
+                    gameStatus = Status.Won;
+                }
+                else if (sumOfDice == (int)DiceNames.Seven)
+                {
+                    gameStatus = Status.Lost;
+                }
+                labelResult.Text += "\nSum of Dices is " + sumOfDice;
+                if (gameStatus == Status.Won)
+                {
+                    labelResult.Text += "\nYou Won ";
+                }
+                else if (gameStatus == Status.Lost)
+                {
+                    labelResult.Text += "\nYou Lost ";
+                }
+                else
+                {
+                    labelResult.Text += "\nGame Continue, Roll Again ";
+                }
+            }
+            else
+            {
+                switch ((DiceNames)sumOfDice)
+                {
+                    case DiceNames.Seven:
+                    case DiceNames.YoLeven:
+                        gameStatus = Status.Won;
+                        break;
+                    case DiceNames.SnakeEyes:
+                    case DiceNames.Trey:
+                    case DiceNames.BoxCars:
+                        gameStatus = Status.Lost;
+                        break;
+                    default:
+                        gameStatus = Status.Continue;
+                        myPoint = sumOfDice;
+                        break;
+                }
+                labelResult.Text += "\nSum of Dices is " + sumOfDice;
+                if (gameStatus == Status.Won)
+                {
+                    labelResult.Text += "\nYou Won ";
+                }
+                else if (gameStatus == Status.Lost)
+                {
+                    labelResult.Text += "\nYou Lost ";
+                }
+                else
+                {
+                    labelResult.Text += "\nYour point is " + sumOfDice + ", Roll Again ";
+                }
+            }
+        }
+
+        private int RollDice()
+        {
+            Random random = new Random();
+            int dice1 = random.Next(1, 7);
+            int dice2 = random.Next(1, 7);
+
+            labelFirstNum.Text = "" + dice1;
+            labelSecNum.Text = "" + dice2;
+            return dice1 + dice2;
+        }
+        
+    }
+    public enum Status { Continue, Won, Lost }
+    public enum DiceNames
+    {
+        SnakeEyes = 2,
+        Trey = 3,
+        Seven = 7,
+        YoLeven = 11,
+        BoxCars = 12
+    }
+}
